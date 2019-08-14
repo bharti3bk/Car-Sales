3b5d4c7b957146f73c1b8c9e44fb29cda4bf7763
bharti3bk <bharti3bk@gmail.com>
2019-08-14 12:21:11 -0700

    Created Action Creators

---
+++ src/store/Actions/index.js
@@ -0,0 +1,15 @@
+
+// Created Action Creators (buyItem and removeFunctionality) which i will pass to the connect function as its second argument of its frist invocation.
+export function buyItem(item) {
+    return{
+    type : "BUY",
+    payload : item
+  }  
+} 
+
+export function removeFunctionality(item) {
+    return {
+        type : "REMOVE" ,
+        payload: item
+    }
+}
